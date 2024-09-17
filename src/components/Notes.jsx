5f51de95da055cd0300e406cc97b44e525e3223e
Sandra Truong <105508619+smn-tru@users.noreply.github.com>
2024-09-16 23:53:26 -0600

    added filler components

---
+++ src/components/Notes.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Notes(){
+    return (
+        <div className='card-element d-flex flex-column'>
+            <div className="">
+                <div className="position-relative">
+                    <div className="px-1 pb-2 pt-3 d-flex align-items-center justify-content-around">
+                        <div className="text-center flex-shrink-0">
+                            Notes
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Notes;
---
+++ src/components/Search.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Search(){
+    return (
+        <div className='card-element d-flex flex-column'>
+            <div className="">
+                <div className="position-relative">
+                    <div className="px-1 pb-2 pt-3 d-flex align-items-center justify-content-around">
+                        <div className="text-center flex-shrink-0">
+                            Search
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Search;
